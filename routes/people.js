979e85eab5332a03795e4628a9f646074921a377
Lumur1333 <200304037@student.georgianc.on.ca>
2020-11-25 15:28:32 -0500

    removed part that doesn't work

--- routes/people.js
+++ routes/people.js
@@ -1,13 +1,3 @@
-// const { index, show, here } = require('../controllers/people');
-
-// module.exports = router => {
-//     router.get('/', index);
-//     router.get('/people', show);
-//     router.get('/people/:id', here);
-
-//     return router;
-// };
-
 const { index, show, here } = require('../controllers/people');
 module.exports = router => {
     router.get('/', index);
